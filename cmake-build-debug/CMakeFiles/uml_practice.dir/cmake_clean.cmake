bed2fc78ada1c5819935bdfa4ccbfd5fd2423b46
estarodubov <estarodubov@team.amocrm.com>
2018-02-23 02:11:33 +0300

    Created Class

---
+++ cmake-build-debug/CMakeFiles/uml_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uml_practice.dir/main.cpp.o"
+  "uml_practice.pdb"
+  "uml_practice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uml_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
